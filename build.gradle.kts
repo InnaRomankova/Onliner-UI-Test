77df0e0b65a642d636d36ff6aa1fad10c4ad77fc
Ina Ramankova <Romankova03071978@gmail.com>
2025-07-08 13:43:22 +0300

    Add dependencies Selenium, JUnit 5, Lombok, Allure, Logging, AspectJ to project (#1)
    * Add dependencies Selenium, JUnit 5, Lombok, Allure, Logging, AspectJ to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    id("io.qameta.allure") version "2.11.2"
 }
 
 group = "org.example"
@@ -10,10 +11,45 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.seleniumhq.selenium:selenium-java:4.33.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+    testImplementation("io.qameta.allure:allure-junit5:2.22.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-api:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.3")
+    runtimeOnly("org.aspectj:aspectjweaver:1.9.21")
 }
 
 tasks.test {
     useJUnitPlatform()
+    ignoreFailures = false
+    finalizedBy("allureReport")
+}
+
+tasks {
+    clean {
+        delete("allure-results")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+allure {
+    version.set("2.22.0")
+    adapter {
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.22.0")
+            }
+        }
+    }
 }
