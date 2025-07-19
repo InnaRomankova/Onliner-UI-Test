bef343a009ca1681dc7eb93489c02bb995165bd0
Ina Ramankova <romankova03071978@gmail.com>
2025-07-19 10:31:16 +0300

    Add annotations @Story, @DisplayName for tests. Add explanation in assertions.

--- src/test/java/by/onliner/tests/CartTest.java
+++ src/test/java/by/onliner/tests/CartTest.java
@@ -3,7 +3,9 @@ package by.onliner.tests;
 import by.onliner.model.pages.MainPage;
 import by.onliner.model.pages.ProductPricingPage;
 import by.onliner.utils.listeners.ScreenshotOnFailureExtension;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +18,8 @@ public class CartTest extends BaseTest {
 
     @Test
     @Tag("sanity")
+    @Story("CartTest")
+    @DisplayName("Adding the product into the cart from product’s page")
     public void testAddingProductIntoCartFromProductsPage() {
         MainPage mainPage = new MainPage();
         mainPage.typeTextInSearchField("Смартфон Samsung Galaxy 4GB/128GB (черный)");
@@ -25,15 +29,17 @@ public class CartTest extends BaseTest {
 
         Assertions.assertAll("Verify the box with product, text 'Product added to cart' and the buttons" +
                         " 'Go to cart' and 'Continue shopping' appeared on the screen",
-                () -> assertTrue(productPricingPage.verifyTextProductAddedToCartIsDisplayed()),
-                () -> assertTrue(productPricingPage.verifyButtonContinueShoppingIsDisplayed()),
-                () -> assertTrue(productPricingPage.verifyButtonGoToCartIsDisplayed()),
-                () -> assertEquals(1, productPricingPage.getIconValueOfCountShoppingCart())
+                () -> assertTrue(productPricingPage.verifyTextProductAddedToCartIsDisplayed(), "Text 'Product added to cart' is not displayed"),
+                () -> assertTrue(productPricingPage.verifyButtonContinueShoppingIsDisplayed(), "Button 'Continue shopping' is not displayed"),
+                () -> assertTrue(productPricingPage.verifyButtonGoToCartIsDisplayed(), "Button 'Go to cart' is not displayed"),
+                () -> assertEquals(1, productPricingPage.getIconValueOfCountShoppingCart(), "An icon value of count shopping cart is not changed on 1")
         );
     }
 
     @Test
-    @Tag("sanity")
+    @Tag("negative")
+    @Story("CartTest")
+    @DisplayName("Adding the product into the cart from product’s page")
     public void testAddingProductFailingForAllureScreenshot() {
         MainPage mainPage = new MainPage();
         mainPage.typeTextInSearchField("Смартфон Samsung Galaxy 4GB/128GB (черный)");
@@ -43,10 +49,10 @@ public class CartTest extends BaseTest {
 
         Assertions.assertAll("Verify the box with product, text 'Product added to cart' and the buttons" +
                         " 'Go to cart' and 'Continue shopping' appeared on the screen",
-                () -> assertTrue(productPricingPage.verifyTextProductAddedToCartIsDisplayed()),
-                () -> assertTrue(productPricingPage.verifyButtonContinueShoppingIsDisplayed()),
-                () -> assertTrue(productPricingPage.verifyButtonGoToCartIsDisplayed()),
-                () -> assertEquals(3, productPricingPage.getIconValueOfCountShoppingCart())
+                () -> assertTrue(productPricingPage.verifyTextProductAddedToCartIsDisplayed(), "Text 'Product added to cart' is not displayed"),
+                () -> assertTrue(productPricingPage.verifyButtonContinueShoppingIsDisplayed(), "Button 'Continue shopping' is not displayed"),
+                () -> assertTrue(productPricingPage.verifyButtonGoToCartIsDisplayed(), "Button 'Go to cart' is not displayed"),
+                () -> assertEquals(3, productPricingPage.getIconValueOfCountShoppingCart(), "An icon value of count shopping cart is not changed on 1")
         );
     }
 }
