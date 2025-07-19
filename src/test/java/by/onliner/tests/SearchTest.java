3e21edda7fe592242cbf19af69e66d3b0bab8c09
Ina Ramankova <Romankova03071978@gmail.com>
2025-07-19 10:34:07 +0300

    Add annotations @Story, @DisplayName for tests. (#8)
    * Add annotations @Story, @DisplayName for tests.
    Add explanation in assertions.

--- src/test/java/by/onliner/tests/SearchTest.java
+++ src/test/java/by/onliner/tests/SearchTest.java
@@ -3,7 +3,9 @@ package by.onliner.tests;
 import by.onliner.model.pages.MainPage;
 import by.onliner.model.pages.SearchedProductPage;
 import by.onliner.utils.listeners.ScreenshotOnFailureExtension;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,12 +15,15 @@ public class SearchTest extends BaseTest {
 
     @Test
     @Tag("sanity")
+    @Story("SearchTest")
+    @DisplayName("Search products ")
     public void testSearchProduct() {
         MainPage mainPage = new MainPage();
         mainPage.typeTextInSearchField("Смартфон Samsung Galaxy 4GB/128GB (черный)");
         SearchedProductPage searchedProductPage = mainPage.selectProductFromListByClickingOnProductName(0);
 
         Assertions.assertTrue(searchedProductPage.isCatalogMastheadContainsText
-                ("(?=.*Samsung)(?=.*Galaxy)(?=.*4GB/128GB)(?=.*\\(черный\\))(?=.*(Телефон|Смартфон)).*"));
+                        ("(?=.*Samsung)(?=.*Galaxy)(?=.*4GB/128GB)(?=.*\\(черный\\))(?=.*(Телефон|Смартфон)).*"),
+                "The page doesn't contains the name of phone");
     }
 }
